7368f5540f355edc71891844943f857bfa730c7f
Maxim Varshavsky <code4rizz@gmail.com>
2025-05-20 22:02:15 -0700

    Linking?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
+<script>
+  import newsData from '$lib/news-list.json';
+
+  const septemberMain = newsData.Headlines.Descriptions["2024-25"].September.main;
+  const octoberMain = newsData.Headlines.Descriptions["2024-25"].October.main;
+</script>
 <h1 class="huge2timesText">AMS News</h1>
 <h4>Welcome to the AMS News Website!</h4>
+<br><br>
+<div id="Months">
+    <h3>May</h3>
+    <br>
+    <h3>April</h3>
+    <br>
+    <h3>March</h3>
+    <br>
+    <h3>Dec-Feb</h3>
+    <br>
+    <h3>November</h3>
+    <br>
+    <h3>October</h3>
+    <h5>{octoberMain}</h5>
+    <br>
+    <h3>September</h3>
+    <h5>{septemberMain}</h5>
+    <br>
+</div>
