9776a7789055024347748b008ac77d19f6a39683
Maxim Varshavsky <code4rizz@gmail.com>
2025-05-22 19:59:42 -0700

    Updates

---
+++ _app/immutable/chunks/t1cMqtgm.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1vxnnv4)==null?void 0:s.base)??"/AMS_News";var e;const t=((e=globalThis.__sveltekit_1vxnnv4)==null?void 0:e.assets)??a;export{t as a,a as b};
