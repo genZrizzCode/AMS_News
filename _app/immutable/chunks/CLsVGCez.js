0f936df22f919d56d31a6fea88055521af0fdb5d
Maxim Varshavsky <code4rizz@gmail.com>
2025-05-23 19:57:48 -0700

    Updates

--- _app/immutable/chunks/CLsVGCez.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_1piff1b)==null?void 0:s.base)??"/AMS_News";var e;const t=((e=globalThis.__sveltekit_1piff1b)==null?void 0:e.assets)??a;export{t as a,a as b};
