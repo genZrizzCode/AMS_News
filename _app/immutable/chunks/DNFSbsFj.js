6650b93b2d3528cf70581161cb7c0b7a8f28a97d
Maxim Varshavsky <code4rizz@gmail.com>
2025-05-25 17:29:52 -0700

    Updates

--- _app/immutable/chunks/DNFSbsFj.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_7fs1bu)==null?void 0:s.base)??"/AMS_News";var e;const t=((e=globalThis.__sveltekit_7fs1bu)==null?void 0:e.assets)??a;export{t as a,a as b};
---
+++ _app/immutable/nodes/4.gCHqoAz2.js
@@ -0,0 +1,1 @@
+import{t,a as p}from"../chunks/DVUx_nIv.js";import"../chunks/D8fJjCzl.js";var n=t("<h1>Coming Soon!</h1>");function m(o){var a=n();p(o,a)}export{m as component};
